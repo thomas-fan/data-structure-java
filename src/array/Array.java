eee8277b711b928081c9fa257ded6b2452ddbbe4
thomas-fan <youxiboy007@vip.qq.com>
2020-04-21 10:05:21 +0800

    完成数组查询

--- src/array/Array.java
+++ src/array/Array.java
@@ -50,6 +50,7 @@ public class Array {
 
     /**
      * 在所有元素前添加一个新元素
+     *
      * @param e
      */
     public void addFirst(int e) {
@@ -80,11 +81,87 @@ public class Array {
             throw new IllegalArgumentException("Add failed. Required index >= 0 and <= size.");
         }
 
-        for (int i = size - 1; i >= index; i --) {
+        for (int i = size - 1; i >= index; i--) {
             data[i + 1] = data[i];
         }
 
         data[index] = e;
-        size ++;
+        size++;
+    }
+
+    /**
+     * 获取 index 索引位置的元素
+     *
+     * @param index
+     * @return
+     */
+    int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        }
+
+        return data[index];
+    }
+
+    /**
+     * 修改index 位置的元素
+     *
+     * @param index
+     * @param e
+     */
+    void set(int index, int e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Index is illegal.");
+        }
+
+        data[index] = e;
+    }
+
+    /**
+     * 判断数组是否包含某个元素
+     *
+     * @param e
+     * @return
+     */
+    public boolean contains(int e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 查找数组中某个元素，找到返回索引，否则返回-1
+     *
+     * @param e
+     * @return int
+     */
+    public int find(int e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Array: size= %d, capacity = %d\n", size, data.length));
+        sb.append('[');
+        for (int i = 0; i < size; i++) {
+            sb.append(data[i]);
+            if (i != size - 1) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append(']');
+        return sb.toString();
     }
 }
--- src/array/Main.java
+++ src/array/Main.java
@@ -2,5 +2,14 @@ package array;
 
 public class Main {
     public static void main(String[] args) {
+        Array arr = new Array(20);
+        for (int i = 0; i < 10; i ++) {
+            arr.addLast(i);
+        }
+
+        arr.add(1, 100);
+        System.out.println(arr);
+        arr.addFirst(-1);
+        System.out.println(arr);
     }
 }
