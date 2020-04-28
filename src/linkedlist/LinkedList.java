eea6e387156dbd4f687b2c81ed81341317d830ac
thomas-fan <youxiboy007@vip.qq.com>
2020-04-28 11:08:07 +0800

    完成链表增加操作

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,72 @@
+package linkedlist;
+
+public class LinkedList<E> {
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void addFirst(E e) {
+        Node node = new Node(e);
+        node.next = head;
+        head = node;
+//        head = new Node(e, head); // 上面三行，等价于
+        size++;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed! Illegal index.");
+        }
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+
+            Node node = new Node(e);
+            node.next = prev.next;
+            prev.next = node;
+//        prev.next = new Node(e, prev.next); // 上面三行，等价于
+        }
+
+        size++;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+}
