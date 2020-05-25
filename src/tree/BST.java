c352a68f0eb491b8d97b10f0f5543644ae5b56f2
thomas <zhangfan@thomas-fan.com>
2020-05-25 18:07:49 +0800

    使用方式二实现二分搜索树添加

--- src/tree/BST.java
+++ src/tree/BST.java
@@ -29,33 +29,52 @@ public class BST<E extends Comparable<E>> {
     }
 
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
-            size++;
-        } else {
-            add(root, e);
-        }
-
+        root = add(root, e);
     }
 
-    private void add(Node node, E e) {
-        if (e.equals(node.e)) {
-            return;
-        } else if (e.compareTo(node.e) < 0 && node.left == null) {
-            node.left = new Node(e);
+    private Node add(Node node, E e) {
+        if (node == null) {
             size++;
-            return;
-        } else if (e.compareTo(node.e) > 0 && node.right == null) {
-            node.right = new Node(e);
-            size++;
-            return;
+            return new Node(e);
+        }else if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        }else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
         }
 
-        if (e.compareTo(node.e) > 0) {
-            add(node.right, e);
-        } else {
-            add(node.left, e);
-        }
-
+        return node;
     }
+
+
+// 方式一添加元素
+//    public void add(E e) {
+//        if (root == null) {
+//            root = new Node(e);
+//            size++;
+//        } else {
+//            add(root, e);
+//        }
+//
+//    }
+//
+//    private void add(Node node, E e) {
+//        if (e.equals(node.e)) {
+//            return;
+//        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+//            node.left = new Node(e);
+//            size++;
+//            return;
+//        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+//            node.right = new Node(e);
+//            size++;
+//            return;
+//        }
+//
+//        if (e.compareTo(node.e) > 0) {
+//            add(node.right, e);
+//        } else {
+//            add(node.left, e);
+//        }
+//
+//    }
 }
