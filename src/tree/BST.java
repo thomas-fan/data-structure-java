3f5a810f557c8674936591750d33331fc1134cd6
thomas <zhangfan@thomas-fan.com>
2020-06-01 23:06:10 +0800

    完成二分搜索树删除任意节点

--- src/tree/BST.java
+++ src/tree/BST.java
@@ -217,6 +217,46 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
+    public Node remove(E e) {
+        return remove(root, e);
+    }
+
+    private Node remove(Node node, E e) {
+        if (node == null) {
+            return null;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else {
+            if (node.left == null) {
+                Node rightNode =  node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            // 当左右子树都不为空,则待删除节点右子树的最小节点作为新节点
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+            node.left = null;
+            node.right = null;
+            return successor;
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
