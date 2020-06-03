d64d6734deb091c77279a9a292a87a3e686234d3
thomas <zhangfan@thomas-fan.com>
2020-06-03 13:32:03 +0800

    基于二分搜索树实现集合

---
+++ src/set/Main.java
@@ -0,0 +1,26 @@
+package set;
+
+import utils.FileOperation;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String [] args) {
+       statistic("pride-and-prejudice.txt");
+       statistic("a-tale-of-two-cities.txt");
+    }
+
+    private static void statistic(String path) {
+        System.out.println(path);
+        ArrayList<String > words = new ArrayList<>();
+        if (FileOperation.readFile(path, words)) {
+            System.out.println("Total words: " + words.size());
+            BSTSet<String > set = new BSTSet<>();
+            for (String word: words) {
+                set.add(word);
+            }
+
+            System.out.println("Total different word: " + set.getSize());
+        }
+    }
+}
