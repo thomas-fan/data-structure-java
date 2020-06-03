025861c2102549dd119460596173744e97feffc9
thomas <zhangfan@thomas-fan.com>
2020-06-03 14:11:43 +0800

    测试bst 实现的 set 和 linkedlist 实现的 set 的性能

--- src/set/Main.java
+++ src/set/Main.java
@@ -6,21 +6,24 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String [] args) {
-       statistic("pride-and-prejudice.txt");
-       statistic("a-tale-of-two-cities.txt");
+       statistic("pride-and-prejudice.txt", new BSTSet<String>());
+       statistic("pride-and-prejudice.txt", new LinkedListSet<>());
+//       statistic("a-tale-of-two-cities.txt", new BSTSet<String>());
     }
 
-    private static void statistic(String path) {
+    private static void statistic(String path, Set<String> set) {
+        long startTime = System.nanoTime();
         System.out.println(path);
         ArrayList<String > words = new ArrayList<>();
         if (FileOperation.readFile(path, words)) {
             System.out.println("Total words: " + words.size());
-            LinkedListSet<String > set = new LinkedListSet<>();
             for (String word: words) {
                 set.add(word);
             }
 
             System.out.println("Total different word: " + set.getSize());
         }
+        long endTime = System.nanoTime();
+        System.out.println("运行时间为: " + (endTime - startTime)/ 1000000000.0 + "秒");
     }
 }
