7a07e468c1f30e437afbcd8c53c87a8dba413b31
thomas <zhangfan@thomas-fan.com>
2020-06-15 12:37:20 +0800

    完成最大堆

--- src/heap/MaxHeap.java
+++ src/heap/MaxHeap.java
@@ -5,6 +5,10 @@ import array.Array;
 public class MaxHeap<E extends Comparable<E>> {
     private Array<E> data;
 
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
     public MaxHeap(int capacity) {
         data = new Array<>(capacity);
     }
@@ -17,15 +21,47 @@ public class MaxHeap<E extends Comparable<E>> {
         return data.isEmpty();
     }
 
-    public void add (E e) {
+    public void add(E e) {
         data.addLast(e);
         siftUp(data.getSize() - 1);
     }
 
     private void siftUp(int k) {
-        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0 ) {
+        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
             data.swap(parent(k), k);
             k = parent(k);
+        }
+    }
+
+    public E findMax() {
+        if (data.getSize() == 0) {
+            throw new IllegalArgumentException("can not findMax when heap is empty");
+        }
+        return data.get(0);
+    }
+
+    public E extractMax() {
+        E ret = findMax();
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+        siftDown(0);
+
+        return ret;
+    }
+
+    private void siftDown(int k) {
+        while (leftChild(k) < data.getSize()) {
+            int j = leftChild(k);
+            if (j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j)) > 0) {
+                j = rightChild(k);
+            }
+
+            if (data.get(k).compareTo(data.get(j)) >= 0) {
+                break;
+            }
+
+            data.swap(k, j);
+            k = j;
         }
     }
 
